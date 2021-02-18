5f0cc7c8d750abdab05b5b25fd43f5d31c66cd95
takashi.suzuki <42316228+k-tokitoh@users.noreply.github.com>
2021-02-18 16:50:20 +0900

    loop games

--- main.go
+++ main.go
@@ -5,6 +5,32 @@ import (
 )
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("===== WELCOME TO YUBISUMA =====")
+	for {
+		game()
+		if !askIfPlayAgain() {
+			return
+		}
+	}
+}
+
+func game() {
+	fmt.Println("game started.")
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(n)
 	return
+}
+
+func askIfPlayAgain() bool {
+	for {
+		fmt.Println("play again? (enter y/n)")
+		var c string
+		fmt.Scan(&c)
+		if c == "y" {
+			return true
+		} else if c == "n" {
+			return false
+		}
+	}
 }
